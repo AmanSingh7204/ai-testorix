59bccf178846ea48f6c0765d569a20f0d38fcd02
AmanSingh7204 <133333684+AmanSingh7204@users.noreply.github.com>
2025-07-23 11:13:52 +0530

    Add .next/static/chunks/src_app_auth_signup_page_tsx_a993527f._.js 1753249431804

---
+++ .next/static/chunks/src_app_auth_signup_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@supabase_node-fetch_browser_3897423d.js",
+  "static/chunks/node_modules_726d4331._.js",
+  "static/chunks/src_65b14c23._.js"
+],
+    source: "dynamic"
+});
